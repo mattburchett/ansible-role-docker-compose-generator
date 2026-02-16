669013d2eb5460cddd1a01999e0416bc75d00213
Matt Burchett <matt@mattburchett.com>
2026-02-16 10:29:20 -0600

    fix list indentation and network_name mapping
    Add custom filter plugin with a PyYAML dumper that properly indents
    list items under their parent key. Also remap network_name to the
    native docker-compose 'name' key.

---
+++ filter_plugins/docker_compose_yaml.py
@@ -0,0 +1,24 @@
+import yaml
+
+
+class IndentedListDumper(yaml.Dumper):
+    def increase_indent(self, flow=False, indentless=False):
+        return super().increase_indent(flow, False)
+
+
+def to_docker_compose_yaml(data, indent=2):
+    return yaml.dump(
+        data,
+        Dumper=IndentedListDumper,
+        default_flow_style=False,
+        indent=indent,
+        allow_unicode=True,
+        sort_keys=False,
+    )
+
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'to_docker_compose_yaml': to_docker_compose_yaml,
+        }
